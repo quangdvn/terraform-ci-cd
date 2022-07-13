b042fcbdc43eb0530f8a0431119aa92bb30beea7
quangdvn <ghitacluber@gmail.com>
2022-07-13 14:00:46 +0900

    Hello (1)

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "amz_linux" {
 
 resource "aws_instance" "ansible_server" {
   ami           = data.aws_ami.amz_linux.id
-  instance_type = "t2.micro"
+  instance_type = "t3.small"
 
   lifecycle {
     create_before_destroy = true
